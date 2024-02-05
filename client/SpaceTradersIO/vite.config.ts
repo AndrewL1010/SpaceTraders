38fc6f581b1553049f009a3777ff83167ca3a5a9
Andrew Lu <omegaw@outlook.com>
2024-02-05 01:54:07 -0800

    updated vite config

--- client/SpaceTradersIO/vite.config.ts
+++ client/SpaceTradersIO/vite.config.ts
@@ -3,18 +3,19 @@ import react from '@vitejs/plugin-react'
 import dns from 'dns'
 // https://vitejs.dev/config/
 
+
 dns.setDefaultResultOrder('verbatim')
 export default defineConfig({
   base: "https://d37ij6ny6hvldl.cloudfront.net",
   plugins: [react()],
-  server: {
-    host: "localhost",
-    port: 5001,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000', 
+  // server: {
+  //   host: "localhost",
+  //   port: 5001,
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:5000', 
   
-      },
-    },
-  }
+  //     },
+  //   },
+  // }
 })
